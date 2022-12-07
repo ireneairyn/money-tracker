817e41b9b756e49792101f2c24e337dbc4bf659b
Irene Aunon <irene@ireneaunion.com>
2022-12-07 20:02:09 +0100

    finishing html and js

--- script.js
+++ script.js
@@ -2,18 +2,37 @@ const form = document.getElementById("form");
 const elAmount = document.getElementById("amount");
 const elDescription = document.getElementById("description");
 const elTransactions = document.getElementById("transactions");
+let elBalance = document.getElementById("balance");
+let elTotalIncome = document.getElementById("income");
+let elTotalExpenses = document.getElementById("expenses");
+
+let totalIncome = 0;
+let totalExpenses = 0;
+let balance = 0;
 
 function submitAddTransaction(event) {
-    event.preventDefault();  
-    const amount = Number(elAmount.value);
-    const description = elDescription.value;
-    console.log({amount, description});
+  event.preventDefault();
+  const amount = Number(elAmount.value);
+  const description = elDescription.value;
+  console.log({ amount, description });
 
-    elTransactions.innerHTML += `<div>${description} ${amount}</div>`
-    elAmount.value = ""
-    elDescription.value = ""
-}  
+  elTransactions.innerHTML += `<div>${description} ${amount}</div>`;
+  elAmount.value = "";
+  elDescription.value = "";
+  elDescription.focus()
+
+  if (amount >= 0) {
+    totalIncome += amount;
+  } else {
+    totalExpenses += amount;
+  }
+
+  balance = totalIncome + totalExpenses;
+
+  elTotalIncome.innerHTML = totalIncome;
+  elTotalExpenses.innerHTML = 0 - totalExpenses;
+  elBalance.innerHTML = balance;
+}
 form.addEventListener("submit", submitAddTransaction);
 
-console.log({form})
-
+console.log({ form });
